eca067982c6f8ce22c050e42b745a43b7a444c95
Vaughn Kottler <vaughnkottler@gmail.com>
2022-04-14 02:16:58 -0700

    Add encoding and decoding directories

--- vcorelib/io/__init__.py
+++ vcorelib/io/__init__.py
@@ -5,6 +5,8 @@ A module exposing data-file encoders and decoders.
 # built-in
 from enum import Enum
 import logging
+from pathlib import Path
+from typing import Callable as _Callable
 from typing import NamedTuple
 from typing import Optional as _Optional
 
@@ -21,13 +23,14 @@ from vcorelib.io.encode import (
     encode_toml,
     encode_yaml,
 )
+from vcorelib.io.types import DEFAULT_DATA_EXT as _DEFAULT_DATA_EXT
 from vcorelib.io.types import DataDecoder as _DataDecoder
 from vcorelib.io.types import DataEncoder as _DataEncoder
 from vcorelib.io.types import DataStream as _DataStream
 from vcorelib.io.types import EncodeResult as _EncodeResult
 from vcorelib.io.types import FileExtension, LoadResult
 from vcorelib.paths import Pathlike as _Pathlike
-from vcorelib.paths import get_file_ext, normalize
+from vcorelib.paths import get_file_ext, get_file_name, normalize
 
 
 class _DataHandle(NamedTuple):
@@ -86,7 +89,7 @@ class DataArbiter:
         self.logger = logger
         self.encoding = encoding
 
-    def decoder(self, ext: str) -> _Optional[_DataDecoder]:
+    def _decoder(self, ext: str) -> _Optional[_DataDecoder]:
         """Look up a decoding routine from a file extension."""
 
         result = None
@@ -110,7 +113,7 @@ class DataArbiter:
             logger = self.logger
 
         result = LoadResult({}, False)
-        decoder = self.decoder(ext)
+        decoder = self._decoder(ext)
         if decoder is not None:
             result = decoder(stream, logger, **kwargs)
         return result
@@ -144,6 +147,38 @@ class DataArbiter:
 
         return result
 
+    def decode_directory(
+        self,
+        pathlike: _Pathlike,
+        logger: logging.Logger = None,
+        require_success: bool = False,
+        path_filter: _Callable[[Path], bool] = None,
+        **kwargs,
+    ) -> LoadResult:
+        """
+        Attempt to decode data files in a directory. Assigns data loaded from
+        each file to a key, returns whether or not any files failed to load
+        and the cumulative time that each file-load took.
+        """
+
+        result = {}
+        path = normalize(pathlike)
+        errors = 0
+        load_time = 0
+
+        for child in filter(
+            path_filter if path_filter is not None else lambda _: True,
+            path.iterdir(),
+        ):
+            load = self.decode(path, logger, require_success, **kwargs)
+            if load.success:
+                result[get_file_name(child)] = load.data
+            errors += int(not load.success)
+            if load.time_ns:
+                load_time += load.time_ns
+
+        return LoadResult(result, errors == 0, load_time)
+
     def encode_stream(
         self,
         ext: str,
@@ -158,7 +193,7 @@ class DataArbiter:
             logger = self.logger
 
         result = False
-        encoder = self.encoder(ext)
+        encoder = self._encoder(ext)
         time_ns = -1
         if encoder is not None:
             time_ns = encoder(data, stream, logger, **kwargs)
@@ -180,7 +215,34 @@ class DataArbiter:
                 get_file_ext(path, maxsplit=1), path_fd, data, logger, **kwargs
             )
 
-    def encoder(self, ext: str) -> _Optional[_DataEncoder]:
+    def encode_directory(
+        self,
+        pathlike: _Pathlike,
+        data: dict,
+        ext: str = _DEFAULT_DATA_EXT,
+        logger: logging.Logger = None,
+        **kwargs,
+    ) -> _EncodeResult:
+        """
+        Encode data to a directory where every key becomes a file with the
+        provided extension. The encoding scheme is implied by the extension.
+        """
+
+        success = True
+        time_ns = 0
+        root = normalize(pathlike)
+
+        for key, item in data.items():
+            result = self.encode(
+                root.joinpath(f"{key}.{ext}"), item, logger, **kwargs
+            )
+            success &= result[0]
+            if result[1]:
+                time_ns += result[1]
+
+        return success, time_ns
+
+    def _encoder(self, ext: str) -> _Optional[_DataEncoder]:
         """Look up an encoding routine from a file extension."""
 
         result = None
