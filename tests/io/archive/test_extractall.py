d109007f4e187ce70165e130fb2a9f9cd3b88af8
Vaughn Kottler <vaughnkottler@gmail.com>
2022-04-19 01:23:26 -0700

    Complete unit test coverage

---
+++ tests/io/archive/test_extractall.py
@@ -0,0 +1,49 @@
+"""
+Test the 'extractall' method from the archive module.
+"""
+
+# built-in
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+# internal
+from tests.resources import get_archives_root
+
+# module under test
+from vcorelib.io.archive import extractall
+from vcorelib.io.types import FileExtension
+
+
+def test_extractall():
+    """Test archive-extracting scenarios."""
+
+    root = get_archives_root()
+    curr = os.getcwd()
+    try:
+        # Verify we can extract all of the expected archives.
+        for archive in ["tar", "tar.bz2", "tar.gz", "tar.lzma", "zip"]:
+            with TemporaryDirectory() as tmpdir:
+                assert extractall(Path(root, f"sample.{archive}"), tmpdir)[
+                    0
+                ], f"Couldn't extract a '{archive}' archive!"
+
+        # Verify we can extract to the working directory.
+        with TemporaryDirectory() as tmpdir:
+            os.chdir(tmpdir)
+            assert extractall(Path(root, "sample.tar"))[0]
+            os.chdir(curr)
+
+        # Verify we can't extract an unknown file.
+        with TemporaryDirectory() as tmpdir:
+            assert not extractall(Path(root, "sample.asdf"), tmpdir)[0]
+    finally:
+        os.chdir(curr)
+
+
+def test_has_archive():
+    """Verify that we can correctly identify when an archive is present."""
+
+    root = get_archives_root()
+    assert FileExtension.has_archive(Path(root, "sample")) is not None
+    assert FileExtension.has_archive(Path(root, "not_sample")) is None
---
+++ tests/io/archive/test_make_archive.py
@@ -0,0 +1,49 @@
+"""
+Test the 'make_archive' method from the archive module.
+"""
+
+# built-in
+import os
+from pathlib import Path
+import shutil
+from tempfile import TemporaryDirectory
+
+# internal
+from tests.resources import get_archives_root
+
+# module under test
+from vcorelib.io.archive import make_archive
+
+
+def test_make_archive():
+    """Test that archives can be created."""
+
+    archive_data = Path(get_archives_root(), "sample")
+    with TemporaryDirectory() as tmpdir:
+        # Copy sample data into the temporary directory.
+        shutil.copytree(archive_data, Path(tmpdir, "sample"))
+
+        # Perform testing while inside the temporary directory.
+        curr = os.getcwd()
+        try:
+            os.chdir(tmpdir)
+
+            src = Path("sample")
+
+            # Test expected successes.
+            assert make_archive(src)[0] is not None
+            assert make_archive(src, "tar")[0] is not None
+            assert make_archive(src, "tar.bz2")[0] is not None
+            assert make_archive(src, "tar.lzma")[0] is not None
+            assert make_archive(src, "tar.xz")[0] is not None
+            assert make_archive(src, "zip")[0] is not None
+            with TemporaryDirectory() as tmpdir_new:
+                assert (
+                    make_archive(src, dst_dir=Path(tmpdir_new))[0] is not None
+                )
+
+            # Test expected failures.
+            assert make_archive(Path("not_sample"))[0] is None
+            assert make_archive(src, "tar.asdf")[0] is None
+        finally:
+            os.chdir(curr)
