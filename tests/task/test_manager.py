f80b863a6ba8482235d82c0ffdb7f08839affdb7
Vaughn Kottler <vaughnkottler@gmail.com>
2022-05-08 04:02:54 -0700

    Minor task manager upgrades

--- tests/task/test_manager.py
+++ tests/task/test_manager.py
@@ -17,3 +17,22 @@ def test_task_manager_basic():
     manager.register(Task("test"), ["a", "b", "c"])
     manager.execute(["test"])
     manager.execute(["test"])
+
+    manager.register_to("a", ["b", "c"])
+    manager.register_to("b", ["c"])
+    manager.execute(["test"])
+
+    manager.finalized = False
+    manager.execute(["test"], init_only=True)
+
+
+def test_task_manager_dry_run():
+    """Test that tasks behave correctly during 'init_only'."""
+
+    manager = TaskManager()
+    manager.register(Task("a"))
+    manager.register(Task("b"))
+    manager.register(Task("c"))
+    manager.register(Task("test"), ["a", "b", "c"])
+    manager.execute(["test"], init_only=True)
+    assert manager.tasks["test"].resolved is False
--- vcorelib/task/manager.py
+++ vcorelib/task/manager.py
@@ -2,9 +2,11 @@
 A simple task management interface.
 """
 
-# built-in
 import asyncio
-from typing import Dict, Iterable, List
+
+# built-in
+from collections import defaultdict
+from typing import Dict, Iterable, Set
 
 # internal
 from vcorelib.task import Task
@@ -20,7 +22,7 @@ class TaskManager:
         """Initialize this task manager."""
 
         self.tasks: Dict[str, Task] = {}
-        self.dependencies: Dict[str, List[str]] = {}
+        self.dependencies: Dict[str, Set[str]] = defaultdict(set)
         self.finalized: bool = False
 
     def register(self, task: Task, dependencies: Iterable[str] = None) -> None:
@@ -29,22 +31,31 @@ class TaskManager:
         self.tasks[task.name] = task
         if dependencies is None:
             dependencies = []
-        self.dependencies[task.name] = list(dependencies)
+        self.dependencies[task.name].update(dependencies)
+        self.finalized = False
 
-    async def finalize(self) -> None:
+    def register_to(self, target: str, dependencies: Iterable[str]) -> None:
+        """Register dependencies to a task by name."""
+        self.register(self.tasks[target], dependencies)
+
+    async def finalize(self, **kwargs) -> None:
         """Register task dependencies while the event loop is running."""
 
         if not self.finalized:
             for task, deps in self.dependencies.items():
-                self.tasks[task].depend_on_all(self.tasks[x] for x in deps)
+                self.tasks[task].depend_on_all(
+                    (self.tasks[x] for x in deps), **kwargs
+                )
             self.finalized = True
 
-    def execute(self, tasks: Iterable[str]) -> None:
+    def execute(self, tasks: Iterable[str], **kwargs) -> None:
         """Execute some set of provided tasks."""
 
         async def executor() -> None:
             """Wait for all of the configured tasks to complete."""
-            await self.finalize()
-            await asyncio.gather(*[self.tasks[x].dispatch() for x in tasks])
+            await self.finalize(**kwargs)
+            await asyncio.gather(
+                *[self.tasks[x].dispatch(**kwargs) for x in tasks]
+            )
 
         asyncio.run(executor())
