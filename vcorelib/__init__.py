044d7a67e62eb7e02c4902f71f9cc401729f00d6
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2022-07-07 16:48:46 -0700

    0.10.7 - Add argument to 'in_dir'
    Also add a new 'tempfile' function.

--- vcorelib/__init__.py
+++ vcorelib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.0.8
-# hash=bb0bc2ef621dd0237ef84bb00e5f3964
+# hash=b93d5331565c2e14782084445c056a77
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A collection of core Python utilities."
 PKG_NAME = "vcorelib"
-VERSION = "0.10.6"
+VERSION = "0.10.7"
 
 DEFAULT_INDENT = 2
 DEFAULT_ENCODING = "utf-8"
--- vcorelib/paths/context.py
+++ vcorelib/paths/context.py
@@ -3,9 +3,11 @@ A module for context managers related to file-system paths.
 """
 
 # built-in
-from contextlib import contextmanager
+from contextlib import contextmanager, suppress
 from os import chdir as _chdir
+from os import makedirs as _makedirs
 from pathlib import Path as _Path
+from tempfile import NamedTemporaryFile as _NamedTemporaryFile
 from typing import Iterator as _Iterator
 
 # internal
@@ -14,12 +16,31 @@ from vcorelib.paths import normalize as _normalize
 
 
 @contextmanager
-def in_dir(path: _Pathlike) -> _Iterator[None]:
+def in_dir(path: _Pathlike, makedirs: bool = False) -> _Iterator[None]:
     """Change the current working directory as a context manager."""
 
     cwd = _Path.cwd()
     try:
-        _chdir(_normalize(path))
+        path = _normalize(path)
+        if makedirs:
+            _makedirs(path, exist_ok=True)
+        _chdir(path)
         yield
     finally:
         _chdir(cwd)
+
+
+@contextmanager
+def tempfile(*args, **kwargs) -> _Iterator[_Path]:
+    """
+    Get a valid path to a temporary file and guarantee that its cleaned up
+    afterwards.
+    """
+
+    with _NamedTemporaryFile(*args, **kwargs) as temp:
+        path = _Path(temp.name)
+    try:
+        yield path
+    finally:
+        with suppress(FileNotFoundError):
+            path.unlink()
