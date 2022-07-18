918f598b21b22e250205186a7d24dd5c6ee89dea
Vaughn Kottler <vaughnkottler@gmail.com>
2022-07-18 00:55:35 -0700

    0.10.9 - Add dict 'merge strategies' interface

--- vcorelib/__init__.py
+++ vcorelib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=2.1.0
-# hash=6dd06af93a44c5a28040698c56b5da54
+# hash=32686e2d6a71f1d11fc3723e7c68fa71
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A collection of core Python utilities."
 PKG_NAME = "vcorelib"
-VERSION = "0.10.8"
+VERSION = "0.10.9"
 
 DEFAULT_INDENT = 2
 DEFAULT_ENCODING = "utf-8"
