e1c161d3250788f36abd5a0aae3d2140e7a84a23
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2022-07-21 14:10:52 -0700

    0.10.15 - Add a MovingAverage class

---
+++ tests/math/analysis/test_average.py
@@ -0,0 +1,28 @@
+"""
+Test the 'math.analysis.average' module.
+"""
+
+# third-party
+from pytest import approx
+
+# module under test
+from vcorelib.math.analysis.average import MovingAverage
+
+
+def test_moving_average_basic():
+    """Test basic functionality of the moving average."""
+
+    average = MovingAverage(10)
+    assert average(10.0) == approx(1.0)
+    assert average(10.0) == approx(2.0)
+    assert average(10.0) == approx(3.0)
+    assert average(10.0) == approx(4.0)
+    assert average(10.0) == approx(5.0)
+    assert average(10.0) == approx(6.0)
+    assert average(10.0) == approx(7.0)
+    assert average(10.0) == approx(8.0)
+    assert average(10.0) == approx(9.0)
+    assert average(10.0) == approx(10.0)
+    assert average(0.0) == approx(9.0)
+    average.resize(100)
+    assert average(100.0) == approx(1.0)
