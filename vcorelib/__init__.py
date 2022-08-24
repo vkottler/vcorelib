8b120aa45cf1b6d3357e046731d521a85ba4031c
Vaughn Kottler <vaughnkottler@gmail.com>
2022-08-24 10:28:19 -0700

    0.11.2 - Add a 'require' arg to normalize

--- vcorelib/__init__.py
+++ vcorelib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.0.8
-# hash=12a1fbf02820d9a26fe20a1e0b759aa8
+# hash=38b53ac4b7232925890823097595bd33
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A collection of core Python utilities."
 PKG_NAME = "vcorelib"
-VERSION = "0.11.1"
+VERSION = "0.11.2"
 
 DEFAULT_INDENT = 2
 DEFAULT_ENCODING = "utf-8"
