dd8d1b047af4bc7773fc8debdc49515d43103bb5
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 02:37:21 +0000

    Adding tarfile member sanitization to extractall()

--- vcorelib/io/archive/__init__.py
+++ vcorelib/io/archive/__init__.py
@@ -44,7 +44,29 @@ def extractall(
         # Extract the tar archive.
         if ext is FileExtension.TAR:
             with tarfile.open(src) as tar:
-                tar.extractall(dst, **extract_kwargs)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar, dst, =extract_kwargs)
             success = True
 
         # Extract the ZIP archive.
