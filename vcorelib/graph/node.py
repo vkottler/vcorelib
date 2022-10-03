e44176bf0f0be8dc37b083349cf415ad1464d5c0
Vaughn Kottler <vaughnkottler@gmail.com>
2022-10-03 00:45:42 -0500

    Fix new typing issues

--- vcorelib/graph/node.py
+++ vcorelib/graph/node.py
@@ -5,10 +5,12 @@ A module for working with graph nodes.
 # built-in
 from io import StringIO as _StringIO
 from typing import TextIO as _TextIO
+from typing import Type as _Type
 
 # internal
 from vcorelib.graph.abc import AbstractDiGraphNode as _AbstractDiGraphNode
 from vcorelib.graph.abc import T as _T
+from vcorelib.graph.abc import V as _V
 from vcorelib.graph.edge import write_attributes as _write_attributes
 from vcorelib.graph.edge import write_node_id as _write_node_id
 
@@ -32,35 +34,59 @@ class DiGraphNode(_AbstractDiGraphNode):
 
         stream.write(";")
 
-    def add_child(self, label: str, node: _T = None, **kwargs) -> _T:
+    def add_child(
+        self,
+        label: str,
+        node: _T = None,
+        graph_kind: _Type[_V] = None,
+        **kwargs
+    ) -> _T:
         """
         Add an edge between this node (source) and the other node
         (destination).
         """
 
+        graph = self.graph(kind=graph_kind)
+
         # Ensure the other node is a vertex in the graph.
-        node = self.graph.handle_node(label, node=node)
-        self.graph.add_edge(self.label, node.label, **kwargs)
+        node = graph.handle_node(label, node=node)
+        graph.add_edge(self.label, node.label, **kwargs)
         return node
 
-    def add_parent(self, label: str, node: _T = None, **kwargs) -> _T:
+    def add_parent(
+        self,
+        label: str,
+        node: _T = None,
+        graph_kind: _Type[_V] = None,
+        **kwargs
+    ) -> _T:
         """
         Add an edge between this node (destination) and the other node
         (source).
         """
 
+        graph = self.graph(kind=graph_kind)
+
         # Ensure the other node is a vertex in the graph.
-        node = self.graph.handle_node(label, node=node)
-        self.graph.add_edge(node.label, self.label, **kwargs)
+        node = graph.handle_node(label, node=node)
+        graph.add_edge(node.label, self.label, **kwargs)
         return node
 
-    def add_parallel(self, label: str, node: _T = None, **kwargs) -> _T:
+    def add_parallel(
+        self,
+        label: str,
+        node: _T = None,
+        graph_kind: _Type[_V] = None,
+        **kwargs
+    ) -> _T:
         """
         Add a parallel edge between two nodes. This instance is used as
         'node1'.
         """
 
+        graph = self.graph(kind=graph_kind)
+
         # Ensure the other node is a vertex in the graph.
-        node = self.graph.handle_node(label, node=node)
-        self.graph.add_parallel(self.label, node.label, **kwargs)
+        node = graph.handle_node(label, node=node)
+        graph.add_parallel(self.label, node.label, **kwargs)
         return node
