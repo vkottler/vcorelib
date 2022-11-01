851f3709287d2deac6f757a1172460ceab15c6d7
Vaughn Kottler <vaughnkottler@gmail.com>
2022-10-31 20:58:37 -0700

    1.1.2 - Namespace update

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
     =====================================
     generator=datazen
     version=3.1.0
-    hash=26df6ece8159aa4daba6c1e988db29da
+    hash=cf86991011e83b50968c42c38939d7e7
     =====================================
 -->
 
-# vcorelib ([1.1.1](https://pypi.org/project/vcorelib/))
+# vcorelib ([1.1.2](https://pypi.org/project/vcorelib/))
 
 [![python](https://img.shields.io/pypi/pyversions/vcorelib.svg)](https://pypi.org/project/vcorelib/)
 ![Build Status](https://github.com/vkottler/vcorelib/workflows/Python%20Package/badge.svg)
--- vcorelib/namespace.py
+++ vcorelib/namespace.py
@@ -99,24 +99,44 @@ class NamespaceMixin:
                 namespace = Namespace(delim=namespace_delim)
             self._namespace = namespace
 
-    def namespace(self, name: str = None, delim: str = None) -> str:
-        """Get a namespace string for this object."""
-        return self._namespace.namespace(name=name, delim=delim)
+    def _normalize_namespace(self, namespace: Namespace = None) -> Namespace:
+        """Return this instance's namespace if one isn't provided."""
 
-    def child_namespace(self, *names: str) -> Namespace:
+        if namespace is None:
+            namespace = self._namespace
+        return namespace
+
+    def namespace(
+        self, name: str = None, delim: str = None, namespace: Namespace = None
+    ) -> str:
+        """Get a namespace string for this object."""
+
+        return self._normalize_namespace(namespace=namespace).namespace(
+            name=name, delim=delim
+        )
+
+    def child_namespace(
+        self, *names: str, namespace: Namespace = None
+    ) -> Namespace:
         """Obtain a child namespace."""
-        return self._namespace.child(*names)
+
+        return self._normalize_namespace(namespace=namespace).child(*names)
 
     @_contextmanager
-    def names_pushed(self, *names: str) -> _Iterator[None]:
+    def names_pushed(
+        self, *names: str, namespace: Namespace = None
+    ) -> _Iterator[None]:
         """Apply some names to this object's namespace as a managed context."""
-        with self._namespace.pushed(*names):
+
+        with self._normalize_namespace(namespace=namespace).pushed(*names):
             yield
 
-    def push_name(self, name: str) -> None:
+    def push_name(self, name: str, namespace: Namespace = None) -> None:
         """Push a name onto the stack."""
-        self._namespace.push(name)
 
-    def pop_name(self, name: str = None) -> str:
+        self._normalize_namespace(namespace=namespace).push(name)
+
+    def pop_name(self, name: str = None, namespace: Namespace = None) -> str:
         """Pop the latest name off the stack."""
-        return self._namespace.pop(name=name)
+
+        return self._normalize_namespace(namespace=namespace).pop(name=name)
