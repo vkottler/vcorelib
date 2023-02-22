7f7f67c03e55f292d1a6f132be035f06c812e6c9
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2023-02-22 14:44:38 -0800

    1.2.5 - Timer log typing

--- vcorelib/__init__.py
+++ vcorelib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.0
-# hash=898279328aca3269b681c67fa5d5c2d1
+# hash=667a45b6e3039f998be6e2945ea7cc28
 # =====================================
 
 """
@@ -10,7 +10,7 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A collection of core Python utilities."
 PKG_NAME = "vcorelib"
-VERSION = "1.2.4"
+VERSION = "1.2.5"
 
 # vcorelib-specific content.
 DEFAULT_INDENT = 2
