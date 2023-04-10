74e0dfd58778108d892e59029705f70e2b179775
Vaughn Kottler <vaughnkottler@gmail.com>
2023-04-10 00:30:05 -0500

    Don't use pkg_resources for test resources

--- tests/resources.py
+++ tests/resources.py
@@ -6,32 +6,22 @@ A module for working with test data.
 from os.path import join
 from pathlib import Path
 
-# third-party
-import pkg_resources
-
 # internal
 from vcorelib.schemas.base import SchemaMap
 from vcorelib.schemas.json import JsonSchemaMap
 
 
-def resource(
-    resource_name: str, *parts: str, valid: bool = True, pkg: str = __name__
-) -> Path:
+def resource(resource_name: str, *parts: str, valid: bool = True) -> Path:
     """Locate the path to a test resource."""
 
-    return Path(
-        pkg_resources.resource_filename(
-            pkg,
-            join(
-                "data", "valid" if valid else "invalid", resource_name, *parts
-            ),
-        )
+    return Path(__file__).parent.joinpath(
+        "data", "valid" if valid else "invalid", resource_name, *parts
     )
 
 
-def get_archives_root(pkg: str = __name__) -> Path:
+def get_archives_root() -> Path:
     """Get the data directory for test archives."""
-    return Path(pkg_resources.resource_filename(pkg, join("data", "archives")))
+    return Path(__file__).parent.joinpath("data", "archives")
 
 
 def get_test_schemas() -> SchemaMap:
