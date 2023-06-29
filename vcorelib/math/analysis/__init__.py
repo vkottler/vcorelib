85d03b390c31efe8cfe0c8b714b6b3c4a535a9d8
Vaughn Kottler <vaughnkottler@gmail.com>
2023-06-29 01:59:56 -0500

    Initial weighted-average implementation

--- vcorelib/math/analysis/__init__.py
+++ vcorelib/math/analysis/__init__.py
@@ -3,13 +3,18 @@ A module for aggregating symbols for import.
 """
 
 # internal
-from vcorelib.math.analysis.average import DEFAULT_DEPTH, MovingAverage
+from vcorelib.math.analysis.average import MovingAverage, MovingSum
+from vcorelib.math.analysis.buffer import DEFAULT_DEPTH, FloatBuffer
 from vcorelib.math.analysis.rate import RateTracker, ns_to_s
 from vcorelib.math.analysis.rate.limiter import RateLimiter
+from vcorelib.math.analysis.weighted import WeightedAverage
 
 __all__ = [
     "MovingAverage",
+    "MovingSum",
+    "WeightedAverage",
     "DEFAULT_DEPTH",
+    "FloatBuffer",
     "ns_to_s",
     "RateTracker",
     "RateLimiter",
--- vcorelib/math/analysis/average.py
+++ vcorelib/math/analysis/average.py
@@ -2,44 +2,57 @@
 A module for working with averages.
 """
 
-# built-in
-from typing import List as _List
+# internal
+from vcorelib.math.analysis.buffer import DEFAULT_DEPTH as _DEFAULT_DEPTH
+from vcorelib.math.analysis.buffer import FloatBuffer as _FloatBuffer
 
-DEFAULT_DEPTH = 10
+
+class MovingSum(_FloatBuffer):
+    """A simple moving sum implementation."""
+
+    def __init__(
+        self, depth: int = _DEFAULT_DEPTH, initial: float = 0.0
+    ) -> None:
+        """Initialize this instance."""
+
+        self.sum: float = initial
+        super().__init__(depth=depth, initial=initial)
+
+    def __call__(self, value: float) -> float:
+        """Update the moving sum."""
+
+        self.sum -= super().__call__(value)
+        self.sum += value
+        return self.sum
+
+    def reset(self, initial: float = 0.0) -> None:
+        """Reset the buffer and sum."""
+
+        super().reset(initial=initial)
+        self.sum = sum(self.data)
 
 
-class MovingAverage:  # pylint: disable=too-many-instance-attributes
+class MovingAverage:
     """A class for managing a moving average of floats."""
 
     def __init__(
-        self, depth: int = DEFAULT_DEPTH, initial: float = 0.0
+        self, depth: int = _DEFAULT_DEPTH, initial: float = 0.0
     ) -> None:
         """Initialize this moving average."""
 
-        self._index: int = 0
-        self._data: _List[float] = []
-        self._sum: float = initial
+        self.buffer = MovingSum(depth=depth, initial=initial)
+
+        self._initialized = False
 
         # Intentionally public members.
-        self.depth: int = depth
-        self.value: float = initial
+        self.average: float = initial
         self.max: float = initial
         self.min: float = initial
 
         self.reset(initial=initial)
-        self._initialized = False
 
-    def __call__(self, value: float) -> float:
-        """Add a new value to the dataset and get the average."""
-
-        # Remove the oldest value.
-        self._sum -= self._data[self._index]
-
-        # Add the new value.
-        self._sum += value
-
-        # Calculate the new average.
-        self.value = self._sum / self.depth
+    def _update_min_max(self, value: float) -> None:
+        """Update min-max tracking based on the provided value."""
 
         # Ensure that max and min don't retain an initial value.
         if not self._initialized:
@@ -50,19 +63,18 @@ class MovingAverage:  # pylint: disable=too-many-instance-attributes
             self.max = max(self.max, value)
             self.min = min(self.min, value)
 
-        # Leave the new value behind.
-        self._data[self._index] = value
-        self._index += 1
-        self._index %= self.depth
+    def __call__(self, value: float) -> float:
+        """Add a new value to the dataset and get the average."""
 
-        return self.value
+        self.average = self.buffer(value) / self.buffer.depth
+        self._update_min_max(value)
+        return self.average
 
     def reset(self, initial: float = 0.0) -> None:
         """Reset the average value."""
 
-        self._data = [initial for _ in range(self.depth)]
-        self._sum = sum(self._data)
-        self.value = self._sum / self.depth
+        self.buffer.reset(initial=initial)
+        self.average = self.buffer.sum / self.buffer.depth
         self.max = initial
         self.min = initial
         self._initialized = False
@@ -70,5 +82,5 @@ class MovingAverage:  # pylint: disable=too-many-instance-attributes
     def resize(self, depth: int, initial: float = 0.0) -> None:
         """Set a new depth for this moving average and reset the value."""
 
-        self.depth = depth
+        self.buffer.resize(depth, initial=initial)
         self.reset(initial=initial)
