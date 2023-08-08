9da2344b8725d5803657632fbf05d30f2e1d7148
Amber Dahlberg <mmbrea@hotmail.com>
2023-08-07 23:10:59 -0500

    Making more progress

--- vcorelib/io/file_writer.py
+++ vcorelib/io/file_writer.py
@@ -1,2 +1,35 @@
-"""class IndentedFileWriter:
-    A class for writing lines to a file while tracking indentations"""
+"""
+A module implementing an interface for writing to variably indented files.
+"""
+
+# from contextlib import contextmanager
+
+
+class IndentedFileWriter:
+    """A class for writing lines to a file and tracking indentation."""
+
+    def __init__(self, indentation=" ") -> None:
+        """Initialize this instance."""
+
+        self.space = " "
+        self.level = 0
+        self.indentation = indentation
+
+    def method_1(self) -> None:
+        """helper method could be created to to create an IndentedFileWriter
+        from a path to a file (probably a context manager.)"""
+
+    def method_2(self) -> None:
+        """method taking the str data for a new line of text to write
+        to the file: first writes the indent (some number of
+        e.g. space characters), then writes the str data (function parameter),
+        then writes a newline character (os.linesep)."""
+
+    def indent(self) -> None:
+        """TODO."""
+        self.level += 1
+
+    def dedent(self) -> None:
+        """TODO."""
+        if self.level > 0:
+            self.level -= 1
