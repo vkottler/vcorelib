bea23501e9857098d33adb9899494fb9319eb9dd
Vaughn Kottler <vaughnkottler@gmail.com>
2023-08-14 21:39:06 -0500

    Attempt to fix test

--- tests/asyncio/test_run_handle_stop_signals.py
+++ tests/asyncio/test_run_handle_stop_signals.py
@@ -9,7 +9,6 @@ import os
 import signal
 import sys
 from time import sleep
-from typing import Callable
 
 # module under test
 from vcorelib.asyncio import all_stop_signals, run_handle_stop
@@ -31,27 +30,22 @@ async def waiter(sig: asyncio.Event, count: int = 1) -> None:
     await asyncio.wait_for(wait_n_events(sig, count=count), 10)
 
 
-def get_sample_app(count: int = 1, signals: bool = True) -> Callable[[], None]:
-    """Get a sample app and provide the number of signals to wait on."""
+def sample_app() -> None:
+    """An application that does nothing."""
 
-    def sample_app() -> None:
-        """An application that does nothing."""
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+    sig = asyncio.Event()
+    run_handle_stop(
+        sig,
+        waiter(sig),
+        signals=list(all_stop_signals()),
+        eloop=loop,
+    )
 
-        sig = asyncio.Event()
-        run_handle_stop(
-            sig,
-            waiter(sig, count=count),
-            signals=list(all_stop_signals()) if signals else None,
-            eloop=loop,
-        )
-
-        # Return 0 if the signal is set, 1 if not.
-        sys.exit(int(not sig.is_set()))
-
-    return sample_app
+    # Return 0 if the signal is set, 1 if not.
+    sys.exit(int(not sig.is_set()))
 
 
 def sample_app_no_signals() -> None:
@@ -67,11 +61,42 @@ def sample_app_no_signals() -> None:
     sys.exit(int(not sig.is_set()))
 
 
+def sample_app_wait_two() -> None:
+    """An application that does nothing."""
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    sig = asyncio.Event()
+    run_handle_stop(
+        sig,
+        waiter(sig, count=2),
+        signals=list(all_stop_signals()),
+        eloop=loop,
+    )
+
+    # Return 0 if the signal is set, 1 if not.
+    sys.exit(int(not sig.is_set()))
+
+
+def sample_app_wait_two_no_signals() -> None:
+    """An application that does nothing."""
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    sig = asyncio.Event()
+    run_handle_stop(sig, waiter(sig, count=2), eloop=loop)
+
+    # Return 0 if the signal is set, 1 if not.
+    sys.exit(int(not sig.is_set()))
+
+
 def test_run_handle_stop_multiple_signals():
     """TODO."""
 
-    for signals in [False, True]:
-        proc = Process(target=get_sample_app(2, signals=signals))
+    for target in [sample_app_wait_two, sample_app_wait_two_no_signals]:
+        proc = Process(target=target)
         proc.start()
 
         assert proc.pid is not None
@@ -86,7 +111,7 @@ def test_run_handle_stop_multiple_signals():
         assert proc.exitcode is not None
 
         # The program exits because of the uncaught keyboard interrupt.
-        if not signals:
+        if target is sample_app_wait_two_no_signals:
             assert abs(proc.exitcode) != 0
         else:
             assert abs(proc.exitcode) in (0, signal.SIGTERM)
@@ -95,7 +120,7 @@ def test_run_handle_stop_multiple_signals():
 def test_run_handle_stop_signals():
     """Test handling various stop signals."""
 
-    proc = Process(target=get_sample_app())
+    proc = Process(target=sample_app)
     proc.start()
     sleep(0.25)
 
